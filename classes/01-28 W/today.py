7cea99541a5bef40a8b3c2aa39251981e9b35ad2
student <student@example.com>
2026-01-28 11:00:20 -0500

    update

---
+++ classes/01-28 W/today.py
@@ -0,0 +1,63 @@
+'''
+x = 5
+y = x + 3
+5 + 7 + 2
+
+x = 5
+if x > 10:
+    print("positive")
+    if y > 5:
+        print("bigger")
+print("done")
+
+name = ""
+if not name:
+    print(name, "check")
+
+
+Comparison of values
+==
+!=
+>
+<
+<=
+>=
+
+Logical Comparisons
+not
+in 
+and
+or
+
+
+if x == 5 and y == 7:
+
+    x==5   y==7 and
+
+x = 7
+y = 0
+
+if x:
+    print(x)
+if y == 0:
+    print(y)
+
+
+x=5
+x==5
+x!=56
+x < 10
+x >= 5
+'''
+
+x = input('Please enter a integer: ')
+print(x)
+
+x = 5
+y = 7
+ 
+temp = x
+x = y
+y = temp
+
+x,y = y,x
