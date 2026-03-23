17c47145996b22897d7013431548cb666a5bc174
student <student@example.com>
2026-03-23 09:54:31 -0400

    update

--- sandbox/DS Project.py
+++ sandbox/DS Project.py
@@ -1,5 +1,20 @@
+# %%
 import pandas as pd
 
-print('hi')
+y1df = pd.read_csv("C:/Users/mpierre2/Downloads/McKenzy Pierre Freshman Year Soccer Game by Game Stats(Test).csv", skiprows=1)
 
-y1df = pd.read_csv("data.csv")
+y1df = y1df.drop(y1df.index[16:22])
+
+y1df
+
+# %%
+y1df.info()
+
+# %%
+y1df.shape
+y1df.columns
+# %%
+y1df["GS"] = (y1df["GP"] == "*").astype(int)
+y1df["GS"].value_counts()
+
+# %%
